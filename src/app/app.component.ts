cfda1f1e29b179085b88ecf2f395e3d51636d222
Joan Vasquez <yoloprogramo22@gmail.com>
2018-04-28 08:55:10 -0400

    28/04/2018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+declare var CKEDITOR: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private data:string;
+  //private isShowError:boolean = false;
+  //@Input() editor:FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    CKEDITOR.replace('content', {
+      on: {
+        change: () => {
+          this.data = [CKEDITOR.instances['content'].getData()][0];
+        }
+      }
+    });
+  }
 }
